8a71e16aae0790eaac610ed2ea78840f3a2ee225
v-alex-dev <vensalex1991@gmail.com>
2023-09-11 17:07:52 +0200

    push for tomorow

---
+++ function/4.1/app.js
@@ -0,0 +1,18 @@
+let length = prompt('What is the length');
+let width = prompt('What is the width');
+let result;
+// le = length
+// wi = width
+function calcSurface (le, wi) {
+	// Si les deux valeurs sont égales il s'agit d'un carré et il suffit faire l'une des deux valeur au carré.
+	if (le === wi) {
+		console.log('It\'s a square');
+		return le^2;
+	}
+	// Si pas retourne la multiplication de la longueur par la largeur.
+	return le*wi;
+}
+
+
+result = calcSurface(length, width);
+console.log(result);
